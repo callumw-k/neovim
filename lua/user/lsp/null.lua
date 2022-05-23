7a66b7344170d276c951024e183e8afabfb8da1a
Callum Wellwood-Kane <cwellkane@gmail.com>
2022-05-23 13:52:25 +1000

    removed redunants

--- lua/user/lsp/null.lua
+++ lua/user/lsp/null.lua
@@ -7,13 +7,12 @@ end
 local formatting = null_ls.builtins.formatting
 -- -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+
 null_ls.setup({
 	debug = false,
 	sources = {
 		formatting.prettier.with({ filetypes = { "javascriptreact", "typescriptreact", "javascript", "typescript" } }),
 		formatting.stylua,
-		formatting.black.with({ extra_args = { "--fast" } }),
-		diagnostics.flake8,
 	},
 	on_attach = function(client)
 		if client.resolved_capabilities.document_formatting then

8de8429b62b3f9e3d6b36d408938b3fc06a0960c
Callum Wellwood-Kane <business@callumwk.com>
2023-03-18 21:28:10 +1100

    tidied up telescope and treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,25 +1,30 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-		opts = {
-			ensure_installed = { "tsx", "typescript", "javascript", "python", "rust", "lua", "css", "regex" },
-			auto_install = true,
-			highlight = {
-				enable = true, -- false will disable the whole extension
-			},
-			ident = { enable = true },
-			rainbow = {
-				enable = true,
-				extended_mode = true,
-				max_file_lines = nil,
-			},
-		},
-	},
-	{
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		"callumw-k/nvim-ts-autotag",
 		"nvim-treesitter/nvim-treesitter-context",
-		config = true,
 	},
+	opts = {
+		highlight = {
+			enable = true,
+		},
+		context_commentstring = {
+			enable = true,
+		},
+		autotag = {
+			enable = true,
+		},
+		indent = { enable = true },
+		rainbow = {
+			enable = true,
+			extended_mode = true,
+			max_file_lines = nil,
+		},
+		ensure_installed = { "tsx", "typescript", "javascript", "python", "rust", "lua", "css", "regex" },
+		auto_install = true,
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
+	end,
 }

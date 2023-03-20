149fe57ea801c408cc17c1703f2f8ec8f5204176
Callum Wellwood-Kane <callum@mudbath.com.au>
2023-03-20 13:43:36 +1100

    updated capabilities

--- lua/lsp/lsp-capabilities.lua
+++ lua/lsp/lsp-capabilities.lua
@@ -4,17 +4,26 @@ local ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not ok then
 	return
 end
-local workspace_capabilities = {
-	workspace = {
-		didChangeWatchedFiles = {
-			dynamicRegistration = true,
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+capabilities.textDocument.completion.completionItem = {
+	documentationFormat = { "markdown", "plaintext" },
+	snippetSupport = true,
+	preselectSupport = true,
+	insertReplaceSupport = true,
+	labelDetailsSupport = true,
+	deprecatedSupport = true,
+	commitCharactersSupport = true,
+	tagSupport = { valueSet = { 1 } },
+	resolveSupport = {
+		properties = {
+			"documentation",
+			"detail",
+			"additionalTextEdits",
 		},
 	},
 }
--- Add additional capabilities supported by nvim-cmp
-local default_capabilities = cmp_nvim_lsp.default_capabilities(vim.lsp.protocol.make_client_capabilities())
-local capabilities = vim.tbl_deep_extend("keep", workspace_capabilities, default_capabilities)
-capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 M.capabilities = capabilities
 
